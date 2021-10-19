1ae85d0e69d16e1140449c5911e6693de2fab25f
Biaginibe <49575566+Biaginibe@users.noreply.github.com>
2021-10-18 23:53:34 -0300

    back-end addition

---
+++ back-end/demo/src/main/java/com/example/demo/User.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name="user")
+public class User implements Serializable {
+    //@Column(name="id"); comentei pq tava dando erro, conversar com o Giovani sobre.
+    @Id
+    private int id;
+
+    private String name;
+    private String email;
+}
