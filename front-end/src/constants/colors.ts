7dd3537c68d3dfdae68a255f7bcbf16ab61ebc32
Eloisa Morais <eloisa.smorais@gmail.com>
2021-10-23 19:17:36 -0300

    add initial theming and colors

---
+++ front-end/src/constants/colors.ts
@@ -0,0 +1,6 @@
+const colors = {
+  SP_PRIMARY: '#17c285',
+  WHITE: '#fff',
+};
+
+export default colors;
---
+++ front-end/src/theme/dark.ts
@@ -0,0 +1,9 @@
+import colors from '../constants/colors';
+
+export default {
+  colors: {
+    loginBackground: colors.SP_PRIMARY,
+    background: '#000',
+    white: colors.WHITE,
+  },
+};
