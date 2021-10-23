e7f07be210eedb9c4d275996ccd3e71aeb67fa8b
Eloisa Morais <eloisa.smorais@gmail.com>
2021-10-23 19:19:12 -0300

    add base button component

---
+++ front-end/src/components/Button.tsx
@@ -0,0 +1,25 @@
+import styled, { css } from 'styled-components';
+import colors from '../constants/colors';
+
+const Button = styled.button<{
+  secondary?: boolean;
+}>`
+  border-radius: 6px;
+  max-width: 200px;
+  padding: 8px 60px;
+  border: 0;
+  margin-top: 24px;
+  cursor: pointer;
+
+  background-color: ${colors.WHITE};
+  color: ${colors.SP_PRIMARY};
+
+  ${(props) =>
+    props.secondary &&
+    css`
+      background-color: ${colors.SP_PRIMARY};
+      color: ${colors.WHITE};
+    `}
+`;
+
+export default Button;
