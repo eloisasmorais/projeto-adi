ab6cf89357dd0615945bba9e99e21bfc90e1655b
Eloisa Morais <eloisa.smorais@gmail.com>
2021-10-23 19:17:15 -0300

    eslint config

--- front-end/.eslintrc.js
+++ front-end/.eslintrc.js
@@ -4,27 +4,29 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
-  parser: "@typescript-eslint/parser",
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["import", "react", "@typescript-eslint", "prettier"],
+  plugins: ['import', 'react', '@typescript-eslint', 'prettier'],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }],
-    "import/extensions": "off",
-    "import/no-unresolved": "off",
-    "no-use-before-define": "off",
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'no-use-before-define': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'off',
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
   },
