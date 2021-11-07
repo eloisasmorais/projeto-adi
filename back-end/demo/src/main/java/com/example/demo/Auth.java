3b542f19f6d02f839835ba7d0f97a1181511714c
Biaginibe <49575566+Biaginibe@users.noreply.github.com>
2021-11-07 20:28:13 -0300

    search function for top 10 artists included.

--- back-end/demo/src/main/java/com/example/demo/Auth.java
+++ back-end/demo/src/main/java/com/example/demo/Auth.java
@@ -10,6 +10,7 @@ import java.util.Base64;
 import java.util.Collections;
 import java.util.HashMap;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("auth")
 public class Auth {
@@ -24,7 +25,6 @@ public class Auth {
 
 
     @PostMapping(value="/getResults", consumes = {MediaType.APPLICATION_JSON_VALUE})
-
     public String getResults(@RequestBody HashMap<String, String> payload){
         String code = payload.get("code");
         String requestURL = "https://accounts.spotify.com/api/token";
@@ -36,13 +36,14 @@ public class Auth {
         String str = REACT_APP_SPOTIFY_CLIENT_ID + ":" + REACT_APP_SPOTIFY_CLIENT_SECRET;
         String encoded = Base64.getUrlEncoder().encodeToString(str.getBytes()); //encodando para ficar igual ao do exemplo
 
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));;
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
         headers.add("Authorization", "Basic "+ encoded);
 
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(requestURL)
                 .queryParam("grant_type", "authorization_code")
                 .queryParam("code", code)
+                .queryParam("scope", "user-top-read")
         .queryParam("redirect_uri", "http://localhost:3000");
 
         HttpEntity<?> entity = new HttpEntity<>(headers);
@@ -52,6 +53,31 @@ public class Auth {
                 HttpMethod.POST,
                 entity,
                 String.class);
+        return response.getBody();
+    }
+
+    @PostMapping(value="/getTopItems", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public String getTopItems(@RequestBody HashMap<String, String> payload){
+        String token = payload.get("token");
+        String requestURL = "https://api.spotify.com/v1/me/top/artists";
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.add("Authorization", "Bearer " + token);
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(requestURL)
+                .queryParam("limit", 10);
+
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        HttpEntity<String> response = restTemplate.exchange(
+                builder.toUriString(),
+                HttpMethod.GET,
+                entity,
+                String.class);
+
         return response.getBody();
     }
 }
--- back-end/demo/src/main/java/com/example/demo/UserResource.java
+++ back-end/demo/src/main/java/com/example/demo/UserResource.java
@@ -1,63 +1,63 @@
-package com.example.demo;
-
-import org.springframework.expression.ExpressionException;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Collections;
-import java.util.List;
-
-@RestController
-@RequestMapping("users")
-public class UserResource {
-
-    private final UserRepository repository;
-
-    public UserResource(UserRepository repository){
-        this.repository = repository;
-    }
-
-    @GetMapping("/all")
-    public  List<User> users(){
-        return repository.findAll();
-    }
-
-    @GetMapping("{id}")
-    public User user(@PathVariable int id) throws Exception{
-
-        User user;
-        user = repository.findById(id).orElseThrow(() -> new Exception());
-
-        return user;
-    }
-
-
-    @PostMapping("save")
-    public User save(@RequestBody User user){
-        User createdUser = repository.save(user);
-        return createdUser;
-    }
-
-    @PatchMapping("update")
-    public User updateUser(@RequestBody User updatedUser) throws Exception{
-        User returnUser = repository.findById(updatedUser.getId())
-                .map( user -> {
-                user.setName(updatedUser.getName());
-                user.setEmail(updatedUser.getEmail());
-                return repository.save(user);
-        }).orElseThrow(()->new Exception());
-
-        return returnUser;
-    }
-
-    @DeleteMapping("{id}")
-    public String delete(@PathVariable int id){
-        repository.deleteById(id);
-        return "Usuario com o id " + id + " deletado com sucesso";
-    }
-
-}
+//'package com.example.demo;
+//
+//import org.springframework.expression.ExpressionException;
+//import org.springframework.http.HttpEntity;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.http.MediaType;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.client.RestTemplate;
+//
+//import java.util.Collections;
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("users")
+//public class UserResource {
+//
+//    private final UserRepository repository;
+//
+//    public UserResource(UserRepository repository){
+//        this.repository = repository;
+//    }
+//
+//    @GetMapping("/all")
+//    public  List<User> users(){
+//        return repository.findAll();
+//    }
+//
+//    @GetMapping("{id}")
+//    public User user(@PathVariable int id) throws Exception{
+//
+//        User user;
+//        user = repository.findById(id).orElseThrow(() -> new Exception());
+//
+//        return user;
+//    }
+//
+//
+//    @PostMapping("save")
+//    public User save(@RequestBody User user){
+//        User createdUser = repository.save(user);
+//        return createdUser;
+//    }
+//
+//    @PatchMapping("update")
+//    public User updateUser(@RequestBody User updatedUser) throws Exception{
+//        User returnUser = repository.findById(updatedUser.getId())
+//                .map( user -> {
+//                user.setName(updatedUser.getName());
+//                user.setEmail(updatedUser.getEmail());
+//                return repository.save(user);
+//        }).orElseThrow(()->new Exception());
+//
+//        return returnUser;
+//    }
+//
+//    @DeleteMapping("{id}")
+//    public String delete(@PathVariable int id){
+//        repository.deleteById(id);
+//        return "Usuario com o id " + id + " deletado com sucesso";
+//    }
+//
+//}
