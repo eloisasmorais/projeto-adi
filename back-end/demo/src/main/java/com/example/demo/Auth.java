7b3e16ddfbd40fa1245f9f40466f2c5fd98ab7bc
Biaginibe <49575566+Biaginibe@users.noreply.github.com>
2021-11-07 23:44:22 -0300

    search function for top 10 tracks included.

--- back-end/demo/src/main/java/com/example/demo/Auth.java
+++ back-end/demo/src/main/java/com/example/demo/Auth.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -30,11 +31,11 @@ public class Auth {
         String requestURL = "https://accounts.spotify.com/api/token";
 
 
-        RestTemplate restTemplate = new RestTemplate();  //criando RestTemplate que é como um axios para nós
-        HttpHeaders headers = new HttpHeaders();  //criando um header p enviar junto da request
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
 
         String str = REACT_APP_SPOTIFY_CLIENT_ID + ":" + REACT_APP_SPOTIFY_CLIENT_SECRET;
-        String encoded = Base64.getUrlEncoder().encodeToString(str.getBytes()); //encodando para ficar igual ao do exemplo
+        String encoded = Base64.getUrlEncoder().encodeToString(str.getBytes());
 
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -56,10 +57,35 @@ public class Auth {
         return response.getBody();
     }
 
-    @PostMapping(value="/getTopItems", consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public String getTopItems(@RequestBody HashMap<String, String> payload){
+    @PostMapping(value="/getTopArtists", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public String getTopItems(@RequestBody @NotNull HashMap<String, String> payload){
         String token = payload.get("token");
         String requestURL = "https://api.spotify.com/v1/me/top/artists";
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.add("Authorization", "Bearer " + token);
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(requestURL)
+                .queryParam("limit", 10);
+
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        HttpEntity<String> response = restTemplate.exchange(
+                builder.toUriString(),
+                HttpMethod.GET,
+                entity,
+                String.class);
+
+        return response.getBody();
+    }
+
+    @PostMapping(value="/getTopTracks", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public String getTopTracks(@RequestBody @NotNull HashMap<String, String> payload){
+        String token = payload.get("token");
+        String requestURL = "https://api.spotify.com/v1/me/top/tracks";
 
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
--- back-end/demo/src/main/java/com/example/demo/DemoApplication.java
+++ back-end/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -13,15 +13,4 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-//	@Configuration
-//	public class CorsConfiguration implements WebMvcConfigurer {
-//
-//		@Override
-//		public void addCorsMappings(CorsRegistry registry) {
-//			registry.addMapping("/**")
-//					.allowedOrigins("http://localhost:3000")
-//					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
-//		}
-//	}
-
 }
